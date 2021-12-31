82a35c4bbff77b40546ac3951f6b9285bd0bbc13
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2021-12-31 19:39:57 +0600

    Backdrop: navbar blur effect added

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,54 @@
 <template>
   <div id="nav">
-    <div style="display: flex;">
+    <div style="display: flex">
       <router-link to="/"><h1>To Do</h1></router-link>
-      <h1 id="profilename" v-if="loggedIn">&nbsp;{{name}}</h1>
+      <h1 id="profilename" v-if="loggedIn">&nbsp;{{ name }}</h1>
     </div>
     <div class="right-options">
       <router-link v-if="loggedIn" to="/">Home</router-link>
-      <router-link v-if="loggedIn" to="/addnote"><i class="fas fa-plus-circle"></i> Add note</router-link>
+      <router-link v-if="loggedIn" to="/addnote"
+        ><i class="fas fa-plus-circle"></i> Add note</router-link
+      >
       <router-link v-if="!loggedIn" to="/login">Login</router-link>
-      <router-link v-if="loggedIn" to="/imp"><i class="fas fa-star"></i></router-link>
-      <router-link v-if="loggedIn" @click="loggedOut" to="/login">Logout</router-link>
+      <router-link v-if="loggedIn" to="/imp"
+        ><i class="fas fa-star"></i
+      ></router-link>
+      <router-link v-if="loggedIn" @click="loggedOut" to="/login"
+        >Logout</router-link
+      >
       <router-link v-if="!loggedIn" to="/register">Register</router-link>
     </div>
   </div>
 
   <div id="navmob">
-    <div style="display: flex;">
+    <div style="display: flex">
       <router-link to="/"><h1>To Do</h1></router-link>
-      <h1 id="profilename" v-if="loggedIn">&nbsp; {{name}}</h1>
+      <h1 id="profilename" v-if="loggedIn">&nbsp; {{ name }}</h1>
     </div>
     <div class="right-options">
       <router-link v-if="loggedIn" to="/">Home</router-link>
-      <router-link v-if="loggedIn" to="/addnote"><i class="fas fa-plus-circle"></i> Add note</router-link>
+      <router-link v-if="loggedIn" to="/addnote"
+        ><i class="fas fa-plus-circle"></i> Add note</router-link
+      >
       <router-link v-if="!loggedIn" to="/login">Login</router-link>
-      <router-link v-if="loggedIn" to="/imp"><i class="fas fa-star"></i></router-link>
-      <router-link v-if="loggedIn" @click="loggedOut" to="/login">Logout</router-link>
+      <router-link v-if="loggedIn" to="/imp"
+        ><i class="fas fa-star"></i
+      ></router-link>
+      <router-link v-if="loggedIn" @click="loggedOut" to="/login"
+        >Logout</router-link
+      >
       <router-link v-if="!loggedIn" to="/register">Register</router-link>
     </div>
   </div>
   <router-view />
-  <Footer/>
+  <Footer />
 </template>
 
 <script setup>
-import { computed, onMounted, } from "vue";
+import { computed, onMounted } from "vue";
 import { useStore } from "vuex";
-import { useRouter } from 'vue-router';
-import Footer from './components/Footer.vue'
+import { useRouter } from "vue-router";
+import Footer from "./components/Footer.vue";
 const store = useStore();
 
 const route = useRouter();
@@ -47,15 +59,14 @@ const name = computed(() => {
   return store.state.auth.name;
 });
 
-
 const loggedOut = () => {
-   store.dispatch('auth/logout');
-   route.push({name: "Login"});
+  store.dispatch("auth/logout");
+  route.push({ name: "Login" });
 };
 
 onMounted(() => {
   document.title = "To Do | App";
-})
+});
 </script>
 
 <style lang="scss">
@@ -74,13 +85,13 @@ onMounted(() => {
   justify-content: space-between;
   align-items: center;
   padding: 12px;
-    position: fixed;
+  position: fixed;
   top: 0;
   width: 100%;
   height: 80px;
   padding-left: 0.75rem;
-    padding-right: 0.75rem;
-
+  padding-right: 0.75rem;
+  backdrop-filter: blur(5px);
   a {
     text-decoration: none;
     font-weight: bold;
